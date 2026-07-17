100a885df085709eebe55ead42faf564c326e0b9
greenthree <86123031+greenthree@users.noreply.github.com>
2026-07-17 12:22:55 +0800

    validate assistant-ui chat runtime (#54)

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom/vitest'
+
+class TestResizeObserver implements ResizeObserver {
+  disconnect() {}
+
+  observe() {}
+
+  unobserve() {}
+}
+
+if (!globalThis.ResizeObserver) {
+  globalThis.ResizeObserver = TestResizeObserver
+}
+
+if (!HTMLElement.prototype.scrollTo) {
+  HTMLElement.prototype.scrollTo = () => undefined
+}
